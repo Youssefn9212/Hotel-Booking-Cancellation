64fe20d3c7a2c3ae53abdf2364e7dcaffda451d5
Youssef Nakhla <98815440+Youssefn9212@users.noreply.github.com>
2024-05-02 18:47:27 +0300

    Update phase_5.py

--- phase_5.py
+++ phase_5.py
@@ -137,10 +137,10 @@ def main():
     distribution_df['GDS'][0],  # Value for 'GDS' distribution channel
 ]).reshape(1, -1)
         
-        data = pd.read_csv('Post-cleaning Data.csv', index_col=0)
-        scaler = StandardScaler()
-        scaler.fit(data)
-        input_norm = scaler.transform(input_data)
+        #data = pd.read_csv('Post-cleaning Data.csv', index_col=0)
+        #scaler = StandardScaler()
+        #scaler.fit(data)
+        #input_norm = scaler.transform(input_data)
         # load model
         model = load('Phase 4 Model.joblib')
         # make prediction
